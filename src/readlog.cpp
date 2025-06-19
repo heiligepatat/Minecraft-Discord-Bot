1de17315bbcaa4d0875b25cb24cc3554c0a01d70
heiligepatat <vincent_mathei@hotmail.com>
2025-06-19 15:54:07 +0200

    readlog.cpp now can read joined players

--- src/readlog.cpp
+++ src/readlog.cpp
@@ -2,19 +2,68 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+
+std::string getusername(const std::string& line) {
+    size_t end = line.size();
+    size_t first_space = end;
+    // Trim trailing whitespace
+    while (first_space > 0 && !(line[first_space - 1] == ' ')) {
+        --first_space;
+    }
+    //username should be
+    std::string username = line.substr(first_space,end);
+
+    return username;
+}
+
+
+
+
 void newlogins(std::vector<std::string>& usernames){
     //Check if position.dat
     std::streampos lastPos = 0;
     std::ifstream posIn("../position.dat");
-    if (posIn >> lastPos) {
-        std::cout << "Resuming from position: " << lastPos << "\n";
-    }
+    long long tmp;
+    if (posIn >> tmp) {
+            lastPos = static_cast<std::streampos>(tmp);
+        }
     posIn.close();   
-    std::ifstream file("input.txt");
+    //Start reading file from last position
+    std::ifstream file("../latest.log");
     if (!file.is_open()) {
-        std::cerr << "Failed to open input.txt\n";
+        std::cerr << "Failed to open latest.log\n";
         return;
     } 
     file.seekg(lastPos);
-        
+
+    std::vector<std::string> logins;
+    std::vector<std::string> logouts;
+    std::string word, line;
+
+     while (std::getline(file, line)) {
+        size_t size = line.size();
+        if (line.substr(size - 22) == " appears in the realm.") {
+            logins.push_back(getusername(line.substr(11,size - 33)));
+        }else if (line.substr(size - 18) == " enters the realm."){
+            logins.push_back(getusername(line.substr(11,size - 29)));
+        }
+    }
+    
+    for (size_t i = 0; i < logins.size(); i++){
+        std::cout << logins[i] << std::endl;
+    }
+    
+    // Save the new file position for next time
+    std::ofstream posOut("position.dat");
+    posOut << file.tellg();  // Save current read position
+    posOut.close();    
+}
+int main(){
+    std::cout << std::endl << "readlog started" << std::endl;
+    std::ofstream posOut("position.dat");
+    posOut << 0;
+    posOut.close();
+
+    std::vector<std::string> usernames;
+    newlogins(usernames);
 }
